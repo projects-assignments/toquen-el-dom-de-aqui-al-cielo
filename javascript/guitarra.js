d6eaf80841ebb93c4b7aea5d89308c61d6dbb6ac
josealbertoperezrego <josealbertoperezrego@gmail.com>
2023-12-01 13:16:03 +0100

    audio guitarra desde piano

--- javascript/guitarra.js
+++ javascript/guitarra.js
@@ -1,9 +1,9 @@
-// Es un array que almacena evento teclado
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n']
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
 const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
-
+const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
@@ -13,9 +13,10 @@ document.addEventListener('keydown', e => {
   if (e.repeat) return
   const key = e.key
   const whiteKeyIndex = WHITE_KEYS.indexOf(key)
- 
-  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
 
+  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
 })
 
 function playNote(key) {
